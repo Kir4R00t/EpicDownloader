bd1adb35b38cc6a968c70d6c2d0ec06ebf3b7c83
Kir4R00t <miki4sa@interia.pl>
2024-12-25 17:11:46 +0100

    now url is properly passed to downloader function

--- EpicDownloader/DownloaderApp/views.py
+++ EpicDownloader/DownloaderApp/views.py
@@ -1,4 +1,16 @@
 from django.shortcuts import render, HttpResponse
+from .utils.download_video import download
+
 
 def home(request):
+    if request.method == 'POST':
+        url = request.POST.get('url')
+        print(f"URL: {url}")
+
+        download(url) # Download video and send to user
+
+    else:
+        print("couldn't get url")
+              
     return render(request, 'index.html')
+
